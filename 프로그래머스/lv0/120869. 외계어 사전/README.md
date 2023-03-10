1f9a2307e15b9ec452881da46248ff7c5545601d
yyoungl <127117707+yyoungl@users.noreply.github.com>
2023-03-10 16:24:01 +0900

    [level 0] Title: 외계어 사전, Time: 0.08 ms, Memory: 33.5 MB -BaekjoonHub

--- 프로그래머스/lv0/120869. 외계어 사전/README.md
+++ 프로그래머스/lv0/120869. 외계어 사전/README.md
@@ -1,6 +1,6 @@
 # [level 0] 외계어 사전 - 120869 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120869) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/120869?language=javascript) 
 
 ### 성능 요약
 
--- 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
+++ 프로그래머스/lv0/120869. 외계어 사전/외계어 사전.js
@@ -2,20 +2,19 @@
 function solution(spell, dic) {
     var answer = 0;
     let dicCheck = [];
-    
+    let count = 0;
     for (d of dic) {
-        let count = 0; 
+        let check = true
         for (s of spell){
             if (d.includes(s)) {
                 count ++;
             }
         }
-        if (count == spell.length){
-        dicCheck.push(d);
-        }
     }
     
-    
+    if (count == spell.length){
+        dicCheck.push(d);
+    }
     return dicCheck.length !== 0 ? 1 : 2 ;
 }
 
