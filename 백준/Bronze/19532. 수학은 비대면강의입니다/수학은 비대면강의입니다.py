167ff536f5f8cc3dbfd3432033c13fad85e2ff98
Young Lee <127117707+yyoungl@users.noreply.github.com>
2023-04-17 01:17:20 +0900

    [Bronze II] Title: 수학은 비대면강의입니다, Time: 44 ms, Memory: 31256 KB -BaekjoonHub

--- 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
+++ 백준/Bronze/19532. 수학은 비대면강의입니다/수학은 비대면강의입니다.py
@@ -1,10 +1,6 @@
 a, b, c, d, e, f = map(int, input().split())
-solution = []
 
-for x in range(-999, 1000):
-    for y in range(-999, 1000):
-        if a * x + b * y == c and d * x + e * y == f:
-            solution = [x, y]
-            break
+x = (c*e-b*f)//(a*e-b*d)
+y = (c*d-a*f)//(b*d-a*e)
 
-print(solution[0], solution[1])
+print(x, y)
