bf4884873e0175a46b97f4370a74c7883cda2ae0
Young Lee <127117707+yyoungl@users.noreply.github.com>
2023-05-16 15:36:04 +0900

    [level 2] Title: 주식가격, Time: 41.61 ms, Memory: 16.9 MB -BaekjoonHub

---
+++ 프로그래머스/lv2/42584. 주식가격/주식가격.py
@@ -0,0 +1,30 @@
+# from collections import deque
+# def solution(prices):
+#     answer = []
+    
+#     prices = deque(prices)
+#     while prices:
+#         c = prices.popleft()
+#         count = 0
+#         for x in prices:
+#             if c > x:
+                
+        
+#     return answer
+from collections import deque
+def solution(prices):       # 코드를 해석해보자면 prices를 큐로 만들고
+    answer = []             # 첫번째 원소를 popleft()로 제거하고 남은 prices와 가격비교
+    prices = deque(prices)  # 더 작은 가격을 찾으면 탈출
+    while prices:
+        c = prices.popleft()
+
+        count = 0
+        for i in prices:
+            if c > i:
+                count += 1
+                break
+            count += 1
+
+        answer.append(count)
+
+    return answer
