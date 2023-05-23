5c818d8af12c5d2f627e667a842c7db72a328b8b
Young Lee <127117707+yyoungl@users.noreply.github.com>
2023-05-23 14:19:37 +0900

    [level 2] Title: 카펫, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/lv2/42842. 카펫/카펫.py
+++ 프로그래머스/lv2/42842. 카펫/카펫.py
@@ -1,6 +1,6 @@
 def solution(brown, yellow):
     answer = []
-    total =  brown + yellow
+
     for i in range(1, yellow+1):
         if yellow % i == 0:
             if 2 * (i + yellow//i) + 4 == brown:
