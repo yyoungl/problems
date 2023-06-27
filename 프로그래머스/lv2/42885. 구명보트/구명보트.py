231c67197ddefc7e8006f5502faf5eba0f0a4d8a
Young Lee <127117707+yyoungl@users.noreply.github.com>
2023-06-27 14:47:08 +0900

    [level 2] Title: 구명보트, Time: 14.69 ms, Memory: 10.6 MB -BaekjoonHub

--- 프로그래머스/lv2/42885. 구명보트/구명보트.py
+++ 프로그래머스/lv2/42885. 구명보트/구명보트.py
@@ -2,22 +2,22 @@ from collections import deque
 
 def solution(people, limit):
     answer = 0
-    people.sort()
-    q = deque(people)
+    people.sort() # 오름차순 정렬
+    q = deque(people) # queue 자료구조로 만들어줌
     
-    while q:
+    while q: # q = 대기열
         if len(q) >= 2:
             if q[0] + q[-1] <= limit:
                 q.pop()
                 q.popleft()
                 answer+=1
             elif q[0] + q[-1] > limit:
-                q.pop()
+                q.pop() # q[-1] 없애는 
                 answer+=1
         else:
-            if q[0] <= limit:
-                q.pop()
-                answer +=1
+
+            q.pop()
+            answer +=1
                 
                 
     
