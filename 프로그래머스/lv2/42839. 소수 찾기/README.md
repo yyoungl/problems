87730fcd4ab1e8eb0a9b266f78be9ecbcacfb9e0
Young Lee <127117707+yyoungl@users.noreply.github.com>
2023-08-02 09:33:44 +0900

    [level 2] Title: 소수 찾기, Time: 1.79 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/lv2/42839. 소수 찾기/README.md
+++ 프로그래머스/lv2/42839. 소수 찾기/README.md
@@ -1,10 +1,10 @@
 # [level 2] 소수 찾기 - 42839 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/42839?language=python3) 
 
 ### 성능 요약
 
-메모리: 79.3 MB, 시간: 16.71 ms
+메모리: 10.5 MB, 시간: 1.79 ms
 
 ### 구분
 
--- 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
+++ 프로그래머스/lv2/42839. 소수 찾기/소수 찾기.py
@@ -1,31 +1,42 @@
 from itertools import permutations
-# 파이썬 permutation 라이브러리 import
+
 
 def solution(numbers):
-    permu = []
+    check = [False] * 7
     numbers = list(numbers)
     answer = []
+    arr = []
+
+    def dfs(string, temp, m):
+        if len(temp) == m:
+            num = int(temp)
+            if num not in arr:
+                arr.append(num)
+        for i in range(len(string)):
+            if not check[i]:
+                check[i] = True
+                temp += string[i]
+                dfs(string, temp, m)
+                check[i] = False
+                temp = temp[0:len(temp)-1]
+
     for i in range(len(numbers)):
-        perms = permutations(numbers, i+1)
-        # numbers에 있는 숫자를 1개부터 len(numbers) 개까지 사용해서 만들 수 있는 순열을 반환
-        # perms = [(1, 2), (2, 1), (1, 2, 3), ...] 이런 형태로 반환됨
-        permu += [''.join(p) for p in perms]
-        # 숫자를 조합해서 만들어야 하기 때문에 string 자료형을 유지하고, join method로 숫자의 형태로 만들어 준다.
-    # print(permu)
-    
+        dfs(numbers, "", i + 1)
+
+
     # permu 에 담긴 숫자들이 소수인지 판별하는 반복문
-    for perm in permu:
+    for perm in arr:
         x = int(perm)
         if x < 2:
             continue
         is_prime = True
         # 이렇게 계산하는 게 국룰이라 들었는데... 왜인진 몰름......
-        for i in range(2, int(x**0.5)+1):
+        for i in range(2, int(x ** 0.5) + 1):
             if x % i == 0:
                 is_prime = False
         if is_prime:
             answer.append(x)
-    
-    return len(set(answer)) 
+
+    return len(set(answer))
     # 여기 set으로 중복을 없애기 과정을 첫 번째 반복문 끝나고 해도 될 것 같다.
     # 중복되는 게 많으면 시간적으로 이득일 듯함!!
