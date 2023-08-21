5dd6b044ed02f5824fd8a5a3156ec7c0866dd36c
Young Lee <127117707+yyoungl@users.noreply.github.com>
2023-08-21 14:46:44 +0900

    [D2] Title: 숫자를 정렬하자, Time: 152 ms, Memory: 20,892 KB -BaekjoonHub

--- SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.java
+++ SWEA/D2/1966. 숫자를 정렬하자/숫자를 정렬하자.java
@@ -4,31 +4,28 @@ import java.io.FileInputStream;
 class Solution
 {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
 		Scanner sc = new Scanner(System.in);
 		int T = sc.nextInt();
-		for (int test_case=1; test_case<=T; test_case++) {
+		for (int test_case = 1; test_case <= T; test_case++) {
 			int N = sc.nextInt();
 			int[] nums = new int[N];
-			for (int i=0; i<N; i++) {
-				nums[i]= sc.nextInt();
+			for (int i = 0; i < N; i++) {
+				nums[i] = sc.nextInt();
 			}
-			for (int i=N-1; i>=0; i--) {
-				for (int j=0; j<i; j++) {
-					if (nums[j] > nums[j+1]) {
+			
+			for (int i=0;i <N; i++) {
+				for (int j=0; j<N; j++) {
+					if (nums[i] < nums[j]) {
 						int temp = nums[j];
-						nums[j] = nums[j+1];
-						nums[j+1] = temp;
-						
+						nums[j] = nums[i];
+						nums[i] = temp;
 					}
 				}
 			}
-			System.out.print("#"+test_case+" ");
-			for (int num: nums) {
-				System.out.print(num+" ");
-			}
-			System.out.println();
 			
+			System.out.print("#"+test_case+" ");
+			for (int n: nums) System.out.print(n+" ");
+			System.out.println();
 		}
 	}
 }
