7eaab540a5b8ce8b6b180d4e297512baa996a354
Young Lee <127117707+yyoungl@users.noreply.github.com>
2023-08-28 10:59:16 +0900

    [Silver III] Title: 주유소, Time: 852 ms, Memory: 156080 KB -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.java
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -5,27 +5,27 @@ public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		int N = sc.nextInt();
-		int[] distance = new int[N-1];
-		int[] price = new int[N-1];
-		for (int i=0; i<N-1; i++) {
+		int[] distance = new int[N - 1];
+		int[] price = new int[N];
+		for (int i = 0; i < N - 1; i++) {
 			distance[i] = sc.nextInt();
 		}
-		
-		for (int i=0;i <N-1; i++) {
+
+		for (int i = 0; i < N; i++) {
 			price[i] = sc.nextInt();
 		}
-		sc.nextInt();
-		
+
 		int answer = 0;
 		int cost = price[0];
-		for (int i=0; i<N-1; i++) {
-			if (cost > price[i]) cost = price[i];
-			answer += distance[i] * cost;
+		for (int i = 0; i < N - 1; i++) {
+			if (cost <= price[i]) answer += cost * distance[i];
+			else {
+				answer += price[i] * distance[i];
+				cost = price[i];
+			}
 		}
 		System.out.println(answer);
-		
-		
-		
+
 	}
 
 }
