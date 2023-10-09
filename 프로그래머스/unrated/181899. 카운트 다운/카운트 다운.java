02f55a3224923f11a081ac5981b30209a60dfb13
Young Lee <127117707+yyoungl@users.noreply.github.com>
2023-10-09 18:08:11 +0900

    [level unrated] Title: 카운트 다운, Time: 0.16 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181899. 카운트 다운/카운트 다운.java
@@ -0,0 +1,15 @@
+import java.util.*;
+
+class Solution {
+    public int[] solution(int start, int end_num) {
+        int[] answer = new int[start-end_num+1];
+        System.out.println((start-end_num+1));
+        
+        for (int i=0; i<start-end_num+1; i++) {
+            answer[i] = start-i;
+        }
+        
+        
+        return answer;
+    }
+}
