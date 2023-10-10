e3754b5324c63fa3882b652639f0eb61ac17f97f
Young Lee <127117707+yyoungl@users.noreply.github.com>
2023-10-10 16:20:56 +0900

    [level unrated] Title: 수 조작하기 2, Time: 653.95 ms, Memory: 369 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181925. 수 조작하기 2/수 조작하기 2.java
@@ -0,0 +1,13 @@
+class Solution {
+    public String solution(int[] numLog) {
+        String answer = "";
+        for (int i=1; i<numLog.length; i++) {
+            int diff = numLog[i]-numLog[i-1];
+            if (diff==1) answer+="w";
+            else if (diff==-1) answer+="s";
+            else if (diff==10) answer+="d";
+            else answer+="a";
+        }
+        return answer;
+    }
+}
