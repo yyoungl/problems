a38c43d6222e44b985048d9f7ceb3208db96c518
Young Lee <127117707+yyoungl@users.noreply.github.com>
2023-10-16 14:34:43 +0900

    [Gold IV] Title: 최단경로, Time: 2004 ms, Memory: 323584 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.java
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -1,4 +1,3 @@
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.PriorityQueue;
@@ -48,24 +47,23 @@ public class Main {
 		}
 
 		PriorityQueue<Node> pq = new PriorityQueue<>();
-
-		for (int end = 1; end <= V; end++) {
-			pq.clear();
-			pq.add(new Node(K, 0));
-			dis[K] = 0;
-
-			while (!pq.isEmpty()) {
-				Node now = pq.poll();
+        pq.add(new Node(K, 0));
+        dis[K] = 0;
+        while (!pq.isEmpty()) {
+            Node now = pq.poll();
 				if (visited[now.num])
 					continue;
 
 				for (Node n : graph[now.num]) {
-					dis[n.num] = Math.min(n.cost + dis[now.num], dis[n.num]);
+                    if (n.cost + dis[now.num] < dis[n.num]) {
+                    dis[n.num] = Math.min(n.cost + dis[now.num], dis[n.num]);
 					pq.add(new Node(n.num, dis[n.num]));
+                    }
+
 				}
 				visited[now.num] = true;
 			}
-		}
+		
 		
 		for (int i=1; i<=V; i++) {
 			if (dis[i] == Integer.MAX_VALUE) System.out.println("INF");
