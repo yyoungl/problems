191d5582fbdb313b7e15d97a27feda8178b24158
Young Lee <127117707+yyoungl@users.noreply.github.com>
2023-10-21 23:24:07 +0900

    [Silver III] Title: N과 M (2), Time: 112 ms, Memory: 12876 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.java
@@ -0,0 +1,35 @@
+
+import java.util.Scanner;
+
+public class Main {
+
+	static int M;
+	static int N;
+	static StringBuilder sb;
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		sb = new StringBuilder();
+		N = sc.nextInt();
+		M = sc.nextInt();
+		int[] result = new int[M];
+		permutation(result, 0, 0);
+		System.out.println(sb.toString());
+	}
+
+	public static void permutation(int[] result, int idx, int start) {
+		if (idx == M) {
+			for (int i : result) {
+				sb.append(i + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+
+		for (int i = start; i < N; i++) {
+				result[idx] = i + 1;
+				permutation(result, idx + 1, i+1);
+		}
+	}
+
+}
