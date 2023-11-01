51669e03b98c43f6426f73b2337ae975bc61607f
Young Lee <127117707+yyoungl@users.noreply.github.com>
2023-11-01 10:29:43 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 196 ms, Memory: 16992 KB -BaekjoonHub

---
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.java
@@ -0,0 +1,37 @@
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		int N = sc.nextInt();
+		int[] nums = new int[N];
+		int[] dp = new int[N];
+		for (int i = 0; i < N; i++)
+			nums[i] = sc.nextInt();
+
+		dp[0] = 1;;
+		int answer = 1;
+		
+		if (N > 1) {
+			
+			for (int i = 1; i < N; i++) {
+				int max = 0;
+				for (int j = 0; j < i; j++) {
+					if (nums[j] < nums[i]) {
+						max = Math.max(dp[j], max);
+					}
+				}
+				dp[i] = max + 1;
+				answer = Math.max(dp[i], answer);
+			}
+			
+		}
+		
+		System.out.println(answer);
+
+	}
+
+}
